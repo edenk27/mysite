ce728377ce47b6eb7703bc347a7a0b4a90ddf599
edenk27 <53970378+edenk27@users.noreply.github.com>
2019-08-11 21:09:35 +0300

    this is the third commit

--- manage.py
+++ manage.py
@@ -2,8 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-C:\Users\EdenBenNun\mysite
-
+#new changes
 def main():
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
     try:
