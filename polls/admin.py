2cbe951467c1c3706f73ec874fb11d78ccd80d9b
edenk27 <53970378+edenk27@users.noreply.github.com>
2019-08-14 09:35:08 +0300

    Change view of the questions and choices in the admin

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,20 @@
 from django.contrib import admin
+from .models import Question, Choice
 
-from .models import Question
 
-admin.site.register(Question)
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ("Question",               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
 
 # Register your models here.
